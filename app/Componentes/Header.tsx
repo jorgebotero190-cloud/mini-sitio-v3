5c637733737d0a0e94560310a8177728680e8edd
jorgebotero190-cloud <jorgebotero190@gmail.com>
2025-11-19 22:08:10 -0500

    feat(footer): agrega botón interactivo con mensaje emergente

--- app/Componentes/Header.tsx
+++ app/Componentes/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
         </div>
           <button
     onClick={mostrarMensaje}
-    className="px-4 py-2 bg-yellow-400 text-black rounded-lg font-semibold hover:bg-yellow-300 transition-colors">
+    className="px-4 py-2 bg-purple-400 text-black rounded-lg font-semibold hover:bg-yellow-300 transition-colors">
   
     ¡Haz clic aquí!
   </button>
