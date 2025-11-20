a362554eabedda08a2a0973776484a4ccc164524
jorgebotero190-cloud <jorgebotero190@gmail.com>
2025-11-19 22:02:09 -0500

    feat(footer): agrega estructura base de la sección

--- app/Componentes/Header.tsx
+++ app/Componentes/Header.tsx
@@ -10,22 +10,23 @@ export default function Header() {
   return (
     <header className="bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-600 text-white p-6 shadow-lg">
       <nav className="flex justify-between items-center">
-        {/* Logo o título */}
+        
         <h1 className="text-2xl font-bold tracking-wide">Mini Sitio</h1>
 
-        {/* Menú fijo */}
+        
         <div className="flex gap-10 text-2xl font-medium">
           <a className="hover:text-yellow-300 transition-colors cursor-pointer">Contacto</a>
           <a className="hover:text-yellow-300 transition-colors cursor-pointer">Conócenos</a>
           <a className="hover:text-yellow-300 transition-colors cursor-pointer">Trabaja</a>
           <a className="hover:text-yellow-300 transition-colors cursor-pointer">Productos</a>
-          <button
-            onClick={mostrarMensaje}
-            className="px-4 py-2 bg-yellow-400 text-black rounded-lg font-semibold hover:bg-yellow-300 transition-colors"
-          >
-            ¡Haz clic aquí!
-          </button>
         </div>
+          <button
+    onClick={mostrarMensaje}
+    className="px-4 py-2 bg-yellow-400 text-black rounded-lg font-semibold hover:bg-yellow-300 transition-colors">
+  
+    ¡Haz clic aquí!
+  </button>
+
       </nav>
     </header>
   );
